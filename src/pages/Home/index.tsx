b0d31977954764c12728d7ef783166b4e35c09b2
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-25 02:03:58 -0300

    [add] home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import style from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <View style={style.container}>
+        
+    </View>
+    );
+}
+
+export default Home;

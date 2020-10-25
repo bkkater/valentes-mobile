20b00108a75a3309427776964cdd735c93f41ed1
Josias Furtado <jo.junior94@gmail.com>
2020-10-25 11:13:02 -0300

    add datepicker

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import { Feather } from '@expo/vector-icons'; 
+import { Feather } from '@expo/vector-icons';
 
-import miniLogo from '../../assets/mini-logo.png'
+import miniLogo from '../../../assets/mini-logo.png'
 
 const Header: React.FC = () => {
   return (
-      <View style={{marginTop: 40, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row'}}>
-          <Image source={miniLogo}/>
-          <Feather name="menu" size={24} color="black" />
-      </View>
+    <View style={{ marginTop: 40, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
+      <Image source={miniLogo} />
+      <Feather name="menu" size={24} color="black" />
+    </View>
   );
 }
 
--- src/components/LandingModal/index.tsx
+++ src/components/LandingModal/index.tsx
@@ -1,6 +1,7 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import { Modal, StyleProp, TouchableHighlight, View, Text, ViewStyle, Image } from 'react-native'
 import { Feather } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import Input from '../Input'
 import miniLogo from '../../../assets/mini-logo.png'
 
@@ -10,92 +11,102 @@ import PrimaryButton from '../PrimaryButton';
 import { useNavigation } from '@react-navigation/native';
 
 interface LandingModalProps {
-	readonly styles?: StyleProp<ViewStyle>
-	readonly modalVisible: boolean
-	setModalVisible: Dispatch<SetStateAction<boolean>>
+  readonly styles?: StyleProp<ViewStyle>
+  readonly modalVisible: boolean
+  setModalVisible: Dispatch<SetStateAction<boolean>>
 }
 
 const LandingModal: React.FC<LandingModalProps> = ({ styles, modalVisible, setModalVisible }) => {
-	const { navigate } = useNavigation();
+  const { navigate } = useNavigation();
+  const [showDatePicker, setShowDatePicker] = useState(false)
+  const [date, setDate] = useState(new Date(1598051730000));
 
-	const handleNavigateToHome = () => {
-		setModalVisible(!modalVisible)
-		navigate('Home')
-	}
+  const handleNavigateToHome = () => {
+    setModalVisible(!modalVisible)
+    navigate('Home')
+  }
 
+  const onChangeDate = (event: any) => {
+    console.warn("event", event)
+    setDate(date)
+  }
 
-	return (
-		<View style={styles}>
-			<Modal
-				animationType="slide"
-				transparent={true}
-				visible={modalVisible}
-				onRequestClose={() => setModalVisible(!modalVisible)}
-			>
-				<View style={style.centeredView}>
-					<View style={style.modalView}>
-						<Image source={miniLogo} style={tailwind('mt-4')} />
-						<View>
+  return (
+    <View style={styles}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+        <View style={style.centeredView}>
+          <View style={style.modalView}>
+            <Image source={miniLogo} style={tailwind('mt-4')} />
+            <View style={tailwind('relative')}>
 
-							<Text style={style.contentText}>Acessar</Text>
-							<View style={style.inputContainer}>
-								<Input
-									label='Digite o código do paciente'
-									type='none'
-									keyboardtype='number-pad'
-									maxlength={6}
-								/>
-								<Feather name="hash" size={24} style={{ ...style.icon, bottom: 30 }} color='#F2B441' />
-							</View>
+              <Text style={style.contentText}>Acessar</Text>
+              <View style={style.inputContainer}>
+                <Input
+                  label='Digite o código do paciente'
+                  type='none'
+                  keyboardtype='number-pad'
+                  maxlength={6}
+                />
+                <Feather name="hash" size={24} style={{ ...style.icon, bottom: 30 }} color='#F2B441' />
+              </View>
 
-							<TouchableHighlight underlayColor="#F3EDE0">
-								<View style={[{ ...style.inputContainer }, tailwind('flex-row justify-between items-center')]}>
-									<Input
-										label='Data de nascimento do paciente'
-										type='none'
-										keyboardtype='default'
-										maxlength={6}
-										style={tailwind('w-10/12')}
-									/>
+              <View style={[{ ...style.inputContainer }, tailwind('flex-row justify-between items-center')]}>
+                <Input
+                  label='Data de nascimento do paciente'
+                  type='none'
+                  keyboardtype='default'
+                  maxlength={6}
+                  style={tailwind('w-10/12')}
+                />
 
-									<View style={style.iconContainer}>
-										<Feather name="calendar" size={24} color='white' />
-									</View>
-								</View>
-							</TouchableHighlight>
-
-							<TouchableHighlight underlayColor="#F3EDE0" >
-								<View style={[{ ...style.inputContainer }, tailwind('flex-row justify-between items-center')]}>
-									<Input
-										label='Data de nascimento do responsável'
-										type='none'
-										keyboardtype='default'
-										maxlength={6}
-										style={tailwind('w-10/12')}
-									/>
-
-									<View style={style.iconContainer}>
-										<Feather name="calendar" size={24} color='white' />
-									</View>
-								</View>
-							</TouchableHighlight>
-
-						</View>
+                <TouchableHighlight onPress={() => setShowDatePicker(!showDatePicker)} underlayColor="#ffc14f" style={style.iconContainer}>
+                  <Feather name="calendar" size={24} color='white' />
+                </TouchableHighlight>
+              </View>
 
 
-						<View style={style.buttons}>
-							<TouchableHighlight underlayColor="#F3EDE0" style={style.goBackButton} onPress={() => setModalVisible(!modalVisible)}>
-								<Text style={{ ...style.buttonText, color: '#333' }}>Voltar</Text>
-							</TouchableHighlight>
+              <View style={[{ ...style.inputContainer }, tailwind('flex-row justify-between items-center')]}>
+                <Input
+                  label='Data de nascimento do responsável'
+                  type='none'
+                  keyboardtype='default'
+                  maxlength={6}
+                  style={tailwind('w-10/12')}
+                />
+                <TouchableHighlight onPress={() => { }} underlayColor="#ffc14f" style={style.iconContainer}>
+                  <Feather name="calendar" size={24} color='white' />
+                </TouchableHighlight>
+              </View>
 
-							<PrimaryButton onPress={handleNavigateToHome}/>
-						</View>
+            </View>
+            {showDatePicker && (<DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode="date"
+              is24Hour={true}
+              display="default"
+              onChange={e => onChangeDate(e)}
+            />
+            )}
 
-					</View>
-				</View>
-			</Modal>
-		</View>
-	)
+            <View style={style.buttons}>
+              <TouchableHighlight underlayColor="#F3EDE0" style={style.goBackButton} onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={{ ...style.buttonText, color: '#333' }}>Voltar</Text>
+              </TouchableHighlight>
+
+              <PrimaryButton onPress={handleNavigateToHome} />
+            </View>
+
+          </View>
+        </View>
+      </Modal>
+    </View>
+  )
 }
 
 LandingModal.displayName = 'LandingModal'
